10ea3ee06c4659d828d237bbc73b5344c5b3e779
Daniil Pivovarov <danyapivovarov@gmail.com>
2017-10-11 13:22:43 +0300

    simple in memory spring security added, also basic html pages: user, admin, home, etc.

--- src/main/java/pesiykot/memorius/configuration/SpringBootWebApplication.java
+++ src/main/java/pesiykot/memorius/configuration/SpringBootWebApplication.java
@@ -3,10 +3,9 @@ package pesiykot.memorius.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import pesiykot.memorius.controller.WelcomeController;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = WelcomeController.class)
+@ComponentScan(basePackages = {"pesiykot.memorius.configuration", "pesiykot.memorius.controller", "pesiykot.memorius.error"})
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
