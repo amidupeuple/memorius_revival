41d82fd5a832f53732f7fe122096a4c715750f17
Daniil Pivovarov <danyapivovarov@gmail.com>
2017-10-12 13:11:33 +0300

    Registration page was added

--- src/main/java/pesiykot/memorius/controller/DefaultController.java
+++ src/main/java/pesiykot/memorius/controller/DefaultController.java
@@ -1,7 +1,12 @@
 package pesiykot.memorius.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+import pesiykot.memorius.dto.UserDto;
 
 @Controller
 public class DefaultController {
@@ -39,5 +44,12 @@ public class DefaultController {
     @GetMapping("/403")
     public String error403() {
         return "/error/403";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
     }
 }
